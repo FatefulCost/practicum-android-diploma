835e33fc445c8f9430527dc75d3e86c5be68a853
FatefulCost <FatefulCost@yandex.ru>
2026-03-21 23:49:36 +0300

    Дополнил networkModule

--- app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/NetworkModule.kt
@@ -1,7 +1,55 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.data.network.ApiService
+import ru.practicum.android.diploma.data.network.NetworkClient
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
-    single<ApiService> { ApiService() }
+    single { provideGson() }
+    single { provideOkHttpClient() }
+    single { provideRetrofit(get(), get()) }
+    single { provideApiService(get()) }
+    single { NetworkClient(get()) }
 }
+
+private fun provideGson(): Gson {
+    return GsonBuilder().setLenient().create()
+}
+
+private fun provideOkHttpClient(): OkHttpClient {
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    return OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+}
+
+private fun provideRetrofit(gson: Gson, client: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+}
+
+private fun provideApiService(retrofit: Retrofit): ApiService {
+    return retrofit.create(ApiService::class.java)
+}
+
