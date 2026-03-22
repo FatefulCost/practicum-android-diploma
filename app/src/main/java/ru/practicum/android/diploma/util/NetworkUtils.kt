73ce3e0cab3fd4ef18e068bdedcdc9b43b1657aa
segn1s <delissat3@gmail.com>
2026-03-22 13:57:58 +0300

    Исправил ошибки detekt

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkUtils.kt
@@ -9,8 +9,10 @@ class NetworkUtils(private val context: Context) {
     fun isNetworkAvailable(): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetwork ?: return false
-        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+        return capabilities?.let {
+            it.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                it.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        } ?: false
     }
 }
