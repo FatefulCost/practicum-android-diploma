225267e52069b10b0394c70b40fb969b8028ef4f
FatefulCost <FatefulCost@yandex.ru>
2026-04-13 17:43:10 +0300

    исправил замечания детект

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -105,7 +104,9 @@ class FilterFragment : Fragment() {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 binding.etSalary.clearFocus()
                 true
-            } else false
+            } else {
+                false
+            }
         }
 
         binding.cbHideWithoutSalary.setOnCheckedChangeListener { _, isChecked ->
